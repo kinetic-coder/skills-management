3cd77994e6f60007da7d4fbffd1dfdb612e00e63
kinetic-coder <ojbullock@googlemail.com>
2020-07-12 15:30:10 +0100

    Added Okta

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -6,37 +6,62 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import { grey } from '@material-ui/core/colors';
+import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    appBar: {
+        backgroundColor: 'primary',
+    },
+    title: {
+        flexGrow: 1,
+    },
+    toolbar: {
+        color: 'secondary',
+    },
 }));
 
-export default function Banner() {
-  const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Skills Management
-          </Typography>
-          <Button color="inherit">Login</Button>
-          <Button color="inherit">Register</Button>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+
+const Banner = () => {
+
+    const classes = useStyles();
+    const history = useHistory();
+
+    const login = () => {
+        history.push('/login');
+    }
+
+    const register = () => {
+        history.push('/register');
+    }
+
+    const goHome = () => {
+        history.push('/');
+    }
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" className={classes.appBar}>
+                <Toolbar className={classes.toolbar}>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title} onClick={goHome}>
+                        Skills Management
+                    </Typography>
+                    <Button color="inherit" onClick={login}>Login</Button>
+                    <Button color="inherit" onClick={register}>Register</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 }
+
+export default Banner;
