48e1d2914c8cba3bb746a8697354ac5425be296f
kinetic-coder <ojbullock@googlemail.com>
2020-07-12 15:31:58 +0100

    CODE-SMELL-FIX : Removed NavBar from Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import NavigationBar from './components/General/NavigationBar';
 import HomeView from './views/Home/HomeView';
 import LoginView from './views/Profile/LoginView';
 
@@ -13,8 +12,6 @@ const Routes = () => {
             <Route path="/" component={HomeView} exact />
             <Route path="/register" component={RegisterView} exact/>
             <Route path="/login" component={LoginView} exact/>
-            {/* <Route path="/artist/:artistId" component={Artist} exact/>
-            <Route path="/" component={Home} exact/> */}
         </Router>
     )
 }
