41af4d13e843f78223c9f4a85858703593e9a060
kinetic-coder <ojbullock@googlemail.com>
2020-07-12 15:34:03 +0100

    CODE-SMELL-FIX: Removed import for Grey from Banner.js

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -6,7 +6,6 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import { grey } from '@material-ui/core/colors';
 import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
