10bb5b0b222320ddf96ed863af7acb7c3710e935
kinetic-coder <ojbullock@googlemail.com>
2020-08-31 08:56:05 +0100

    Updates

---
+++ precache-manifest.a8022d7138979e6e6d800e4ecbd554b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eabaa02e276179ec3cb72ab1a23ddf39",
+    "url": "/skills-management/index.html"
+  },
+  {
+    "revision": "fb529c2dc639c0de13b8",
+    "url": "/skills-management/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "67933ba56f0c6ce8d084",
+    "url": "/skills-management/static/js/2.0325b433.chunk.js"
+  },
+  {
+    "revision": "590efd5307b16847952731eb0a4b733c",
+    "url": "/skills-management/static/js/2.0325b433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb529c2dc639c0de13b8",
+    "url": "/skills-management/static/js/main.e33e9f46.chunk.js"
+  },
+  {
+    "revision": "d4fc8c506607ca3a6011",
+    "url": "/skills-management/static/js/runtime-main.cef1ba8c.js"
+  }
+]);
